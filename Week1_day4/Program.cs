dfb06479fd6fbed13b372b8b435e88a688988083
NPawloski <natepawloski@gmail.com>
2017-09-28 14:53:43 -0400

    went through the days lesson

--- Week1_day4/Program.cs
+++ Week1_day4/Program.cs
@@ -10,8 +10,39 @@ namespace Week1_day4
     {
         static void Main(string[] args)
         {
+            /*
+            Console.WriteLine("This is a tab \t.");
+            Console.WriteLine("This is a \"quote\".");
+            Console.WriteLine("This is a backslash \\");
+            Console.WriteLine("This is a \n new line.");
+            Console.WriteLine("This is a bell \a"); */
 
+            //String firstName = "Nathan";
+            //String catName = "Sonic";
 
+            //Console.WriteLine("My name is {0} and my cat is {1}", firstName, catName);
+
+            //for (int i = 0; i < 10; i++)
+            //{
+            //    Console.WriteLine("bell \a");
+            //}
+
+            // String bestFriends = firstName + " & " + catName;
+
+            // Console.WriteLine(bestFriends);
+
+            //bestFriends = String.Concat(firstName, " ", catName);
+
+            //Console.WriteLine(bestFriends);
+
+            //int nameLength = firstName.Length;
+
+            //Console.WriteLine(nameLength);
+            //Console.WriteLine(bestFriends.Length);
+
+            //String firstNameUpper = firstName.ToUpper();
+
+            //Console.WriteLine(firstNameUpper);
 
         }
     }
